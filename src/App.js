fc305f31d4994ba7eb36148b6988887c86ddf9b0
Wengel G <wengelg@Wengels-MacBook-Pro.local>
2020-01-30 15:08:32 -0800

    Add the delete functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+// import React from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="Things to do">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { useState } from 'react';
+import TodoList from './TodoList'
+
+export default function App() {
+  const [todos, setTodos] = useState([
+    {id: 1, content: 'Meet Masao for Udon'},
+    {id: 2, content: 'Fix up these horrible React notes'},
+  ]);
+  function deleteTodo(id) {
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }
   return (
     <div className="App">
-      <header className="Things to do">
-        <p>
-          Edit <code>src/App.js</code> and save to reload!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Things To Do</h1>
+      <TodoList todos={todos} delter={deleteTodo} />
     </div>
   );
 }
-
-export default App;
---
+++ src/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function TodoList({ todos, deleter }) {
+  const todoList = todos.length ? (
+    todos.map(todo => {
+        return (
+            <div key={todo.id}>
+                <span onClick={() => deleter(todo.id)}>{todo.content}</span>
+            </div>
+        );
+    })
+  ) : (
+    <p>No todos left</p>
+  );
+  return <div>{todoList}</div>;
+}
